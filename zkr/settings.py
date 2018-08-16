25fef84a47e4b4c74e88b34995748a2158e8c667
Karolis Vycius <k.vycius@gmail.com>
2018-08-16 11:20:04 +0300

    Delete useless task

--- zkr/settings.py
+++ zkr/settings.py
@@ -182,10 +182,6 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 
 CELERY_BEAT_SCHEDULE = {
-    'worker_heartbeat_default': {
-        'task': 'seimas.tasks.fetch_terms',
-        'schedule': crontab(minute='0', hour='12')
-    },
     'fetch_politicians': {
         'task': 'seimas.tasks.fetch_politicians',
         'schedule': crontab(minute='0', hour='*')
