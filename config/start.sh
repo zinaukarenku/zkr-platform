dd7be7db44dfd61ab0ac5b61d142f4d8c7bb9d2e
Karolis Vycius <k.vycius@gmail.com>
2018-08-19 17:19:14 +0300

    Enable threads

--- config/start.sh
+++ config/start.sh
@@ -8,4 +8,4 @@ echo "==> Django setup, executing: migrate"
 python manage.py migrate
 
 echo "==> Starting uWSGI ..."
-gunicorn zkr.wsgi --bind 0.0.0.0:8080 --workers 5
+gunicorn zkr.wsgi --bind 0.0.0.0:8080 --enable-threads --workers 5
