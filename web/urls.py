07014680055dccf8fd3ac9bb739159bbc4322b98
Karolis Vycius <k.vycius@gmail.com>
2018-08-27 22:04:31 +0300

    Revert "Uncomment index page"
    This reverts commit 14f085134f1ddf8f02527ba4749baa3eda8f91fd.

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from web.views import health_check, subscribe, user_profile, index
+from seimas.views import politician_game
+from web.views import health_check, subscribe, user_profile
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', politician_game, name="seimas_politician_game"),
     path('accounts/profile/', user_profile, name="user_profile"),
     path('accounts/social/connections/', RedirectView.as_view(pattern_name="user_profile", permanent=False)),
     path('health/', health_check, name="health_check"),
