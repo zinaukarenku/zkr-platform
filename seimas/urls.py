18bcd212e20eac5783efdc1fbba0d652cb71c12f
Karolis Vycius <k.vycius@gmail.com>
2018-08-28 12:01:43 +0300

    Empty url links to game

--- seimas/urls.py
+++ seimas/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from seimas.views import politicians, politician, politician_game
+from seimas.views import politicians, politician
 
 urlpatterns = [
-    path('zaidimas/', politician_game, name="seimas_politician_game"),
     path('politikai/', politicians, name="seimas_politicians"),
     path('politikai/<slug:slug>/', politician, name="seimas_politician")
 ]
