cb52da9706ecc9ca82857af9e45bbe9204c0a6f8
Karolis Vycius <k.vycius@gmail.com>
2018-09-29 11:47:19 +0300

    Show last update date

--- zkr/settings.py
+++ zkr/settings.py
@@ -255,6 +255,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
