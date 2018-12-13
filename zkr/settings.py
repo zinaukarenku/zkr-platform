b49b3000f5b71b5c10c68f9198f3d4cf4766c00e
Karolis Vycius <k.vycius@gmail.com>
2018-12-13 22:19:19 +0200

    Add middleware

--- zkr/settings.py
+++ zkr/settings.py
@@ -90,6 +90,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'reversion.middleware.RevisionMiddleware'
 ]
 
 AUTH_USER_MODEL = 'web.User'
