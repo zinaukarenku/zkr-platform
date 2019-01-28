aef23d9556f2df9faa111c9e3de59e5fbaae3852
augis911 <augustinas.butkus.91@gmail.com>
2019-01-28 03:33:40 +0200

    filters-mobile-ui

--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -4,6 +4,10 @@ $(document).ready(function () {
     const openMenuButton = d.querySelector('#open_menu');
     const closeMenuButton = d.querySelector('#close_menu');
     const mobileNav = d.body.querySelector('#mobile_nav');
+    const filtersPoliticianButton = d.body.querySelector('#filtersPoliticianButton');
+    const submitPoliticianFilterButton = d.body.querySelector('#submit-id-filter');
+    const filtersPoliticiansCard = d.body.querySelector('#filtersPoliticiansCard');
+    const closeFilters = d.body.querySelector('#close_filters');
 
     function toggleMobileMenu() {
         if (mobileNav.classList.contains('d-none')) {
@@ -21,9 +25,26 @@ $(document).ready(function () {
         }
     }
 
+    function togglePoliticianFilters() {
+        if (filtersPoliticiansCard.classList.contains('d-none')) {
+            body.classList.add('overflow-hidden');
+            filtersPoliticiansCard.classList.remove('d-none');
+            filtersPoliticiansCard.classList.add('d-sm-block');
+        } else {
+            body.classList.remove('overflow-hidden');
+            filtersPoliticiansCard.classList.remove('d-sm-block');
+            filtersPoliticiansCard.classList.add('d-none');
+        }
+    }
+
 
     openMenuButton.addEventListener('click', toggleMobileMenu);
     closeMenuButton.addEventListener('click', toggleMobileMenu);
+
+    if (window.screen.width < 992) {
+        filtersPoliticianButton.addEventListener('click', togglePoliticianFilters);
+        closeFilters.addEventListener('click', togglePoliticianFilters);
+    }
 
     $('.subscribe-form').each(function () {
         let subscribeForm = $(this);
