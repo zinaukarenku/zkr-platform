d23388e8d8ce5589cb3f64ecc4565acd93159c05
Karolis Vycius <k.vycius@gmail.com>
2019-02-20 23:04:51 +0200

    Celery patch via Datdog

--- zkr/celery_setup.py
+++ zkr/celery_setup.py
@@ -1,9 +1,11 @@
 import os
 import celery
+from ddtrace import patch
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zkr.settings')
 
+patch(celery=True)
 app = celery.Celery('zkr')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
