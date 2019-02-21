66cc36e9d9021feeb9d65a0f6bfb5407794fef00
Karolis Vycius <k.vycius@gmail.com>
2019-02-21 14:51:40 +0200

    Move non Jquery js code out of ready

--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -1,52 +1,4 @@
 $(document).ready(function () {
-    const body = document.querySelector('body');
-    const openMenuButton = document.querySelector('#open_menu');
-    const closeMenuButton = document.querySelector('#close_menu');
-    const mobileNav = document.body.querySelector('#mobile_nav');
-    const filtersMobileButton = document.body.querySelector('#filters_mobile_button');
-    const filtersCard = document.body.querySelector('#filters_card');
-    const closeFilters = document.body.querySelector('#close_filters');
-
-
-    function toggleMobileMenu() {
-        if (mobileNav.classList.contains('d-none')) {
-            body.classList.add('overflow-hidden');
-            mobileNav.classList.remove('d-none');
-            mobileNav.classList.add('d-sm-block');
-            openMenuButton.classList.add('d-none');
-            closeMenuButton.classList.remove('d-none');
-        } else {
-            body.classList.remove('overflow-hidden');
-            mobileNav.classList.remove('d-sm-block');
-            mobileNav.classList.add('d-none');
-            openMenuButton.classList.remove('d-none');
-            closeMenuButton.classList.add('d-none');
-        }
-    }
-
-    function togglePoliticianFilters() {
-        if (filtersCard.classList.contains('d-none')) {
-            body.classList.add('overflow-hidden');
-            filtersCard.classList.remove('d-none');
-            filtersCard.classList.add('d-sm-block');
-        } else {
-            body.classList.remove('overflow-hidden');
-            filtersCard.classList.remove('d-sm-block');
-            filtersCard.classList.add('d-none');
-        }
-    }
-
-    if (openMenuButton !== null && closeMenuButton !== null) {
-        openMenuButton.addEventListener('click', toggleMobileMenu);
-        closeMenuButton.addEventListener('click', toggleMobileMenu);
-    }
-
-    if (filtersMobileButton !== null && closeFilters !== null && window.screen.width < 992) {
-        filtersMobileButton.addEventListener('click', togglePoliticianFilters);
-        closeFilters.addEventListener('click', togglePoliticianFilters);
-    }
-
-
     $('.subscribe-form').each(function () {
         let subscribeForm = $(this);
         subscribeForm.submit(function () {
@@ -64,20 +16,52 @@ $(document).ready(function () {
             return false;
         });
     });
-
-
 });
 
 
-//Google maps for debates
-//const mapContainer = document.body.querySelector('#map');
-//
-//if (mapContainer !== null) {
-//
-//    function initMap() {
-//        let lithuania = {lat: 55.1267529, lng: 23.9179474};
-//        let map = new google.maps.Map(mapContainer, {zoom: 8.07, center: lithuania})
-//    }
-//
-//    initMap();
-//}
+const body = document.querySelector('body');
+const openMenuButton = document.querySelector('#open_menu');
+const closeMenuButton = document.querySelector('#close_menu');
+const mobileNav = document.body.querySelector('#mobile_nav');
+const filtersMobileButton = document.body.querySelector('#filters_mobile_button');
+const filtersCard = document.body.querySelector('#filters_card');
+const closeFilters = document.body.querySelector('#close_filters');
+
+
+function toggleMobileMenu() {
+    if (mobileNav.classList.contains('d-none')) {
+        body.classList.add('overflow-hidden');
+        mobileNav.classList.remove('d-none');
+        mobileNav.classList.add('d-sm-block');
+        openMenuButton.classList.add('d-none');
+        closeMenuButton.classList.remove('d-none');
+    } else {
+        body.classList.remove('overflow-hidden');
+        mobileNav.classList.remove('d-sm-block');
+        mobileNav.classList.add('d-none');
+        openMenuButton.classList.remove('d-none');
+        closeMenuButton.classList.add('d-none');
+    }
+}
+
+function togglePoliticianFilters() {
+    if (filtersCard.classList.contains('d-none')) {
+        body.classList.add('overflow-hidden');
+        filtersCard.classList.remove('d-none');
+        filtersCard.classList.add('d-sm-block');
+    } else {
+        body.classList.remove('overflow-hidden');
+        filtersCard.classList.remove('d-sm-block');
+        filtersCard.classList.add('d-none');
+    }
+}
+
+if (openMenuButton !== null && closeMenuButton !== null) {
+    openMenuButton.addEventListener('click', toggleMobileMenu);
+    closeMenuButton.addEventListener('click', toggleMobileMenu);
+}
+
+if (filtersMobileButton !== null && closeFilters !== null && window.screen.width < 992) {
+    filtersMobileButton.addEventListener('click', togglePoliticianFilters);
+    closeFilters.addEventListener('click', togglePoliticianFilters);
+}
